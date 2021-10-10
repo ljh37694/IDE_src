a627f2290209e8a92dbd94058b071db8563bd527
ljh37694 <ljh37694@naver.com>
2021-10-10 21:04:39 +0900

    중위수 필터(이게 맞는 지는 모르겠습니다)

--- 09_example_1/09_example_1.ino
+++ 09_example_1/09_example_1.ino
@@ -8,27 +8,31 @@
 #define INTERVAL 25 // sampling interval (unit: ms)
 #define _DIST_MIN 100 // minimum distance to be measured (unit: mm)
 #define _DIST_MAX 300 // maximum distance to be measured (unit: mm)
+#define _DIST_ALPHA 0.01 // EMA weight of new sample (range: 0 to 1). Setting this value to 1 effectively disables EMA filter.
 
 // global variables
 float timeout; // unit: us
-float dist_min, dist_max, dist_raw; // unit: mm
+float dist_min, dist_max, dist_raw, dist_ema, alpha; // unit: mm
 unsigned long last_sampling_time; // unit: ms
 float scale; // used for pulse duration to distance conversion
+int a = 3, b = 10, c = 30;
+float num[30] = {0,};
+float median_a, median_b, median_c;
 
 void setup() {
 // initialize GPIO pins
   pinMode(PIN_LED,OUTPUT);
   pinMode(PIN_TRIG,OUTPUT);
-  digitalWrite(PIN_TRIG, LOW); 
+  digitalWrite(PIN_TRIG, LOW);
   pinMode(PIN_ECHO,INPUT);
 
 // initialize USS related variables
   dist_min = _DIST_MIN; 
   dist_max = _DIST_MAX;
+  alpha = _DIST_ALPHA;
   timeout = (INTERVAL / 2) * 1000.0; // precalculate pulseIn() timeout value. (unit: us)
   dist_raw = 0.0; // raw distance output from USS (unit: mm)
   scale = 0.001 * 0.5 * SND_VEL;
-
 // initialize serial port
   Serial.begin(57600);
 
@@ -43,23 +47,36 @@ void loop() {
 
 // get a distance reading from the USS
   dist_raw = USS_measure(PIN_TRIG,PIN_ECHO);
-
+  dist_ema = ((1 - alpha) * dist_ema) + alpha * dist_raw;
+  median_a = Median_filter(a);
+  median_b = Median_filter(b);
+  median_c = Median_filter(c);
+      
 // output the read value to the serial port
-  Serial.println((abs(200 - dist_raw) * n));
   Serial.print("Min:0,");
   Serial.print("raw:");
   Serial.print(dist_raw);
   Serial.print(",");
-  Serial.println("Max:400");
+  Serial.print("median(3):");
+  Serial.print(map(median_a,0,400,100,500));
+  Serial.print(",");
+  Serial.print("median(10):");
+  Serial.print(map(median_b,0,400,200,600));
+  Serial.print(",");
+  Serial.print("median(30):");
+  Serial.print(map(median_c,0,400,300,700));
+//  Serial.print(map(dist_ema,0,400,100,500));
+  Serial.print(",");
+  Serial.println("Max:500");
 
 // turn on the LED if the distance is between dist_min and dist_max
-  if (dist_raw < dist_min || dist_raw > dist_max){
+  if(dist_raw < dist_min || dist_raw > dist_max) {
     analogWrite(PIN_LED, 255);
   }
-  else{
-    analogWrite(PIN_LED, (abs(200 - dist_raw) * 3));
+  else {
+    analogWrite(PIN_LED, 0);
   }
-  
+
 // update last sampling time
   last_sampling_time += INTERVAL;
 }
@@ -74,14 +91,14 @@ float USS_measure(int TRIG, int ECHO)
   reading = pulseIn(ECHO, HIGH, timeout) * scale; // unit: mm
   if(reading < dist_min || reading > dist_max) reading = 0.0; // return 0 when out of range.
   return reading;
-  // Pulse duration to distance conversion example (target distance = 17.3m)
-  // - round trip distance: 34.6m
-  // - expected pulse duration: 0.1 sec, or 100,000us
-  // - pulseIn(ECHO, HIGH, timeout) * 0.001 * 0.5 * SND_VEL
-  //           = 100,000 micro*sec * 0.001 milli/micro * 0.5 * 346 meter/sec
-  //           = 100,000 * 0.001 * 0.5 * 346 * micro * sec * milli * meter
-  //                                           ----------------------------
-  //                                           micro * sec
-  //           = 100 * 173 milli*meter = 17,300 mm = 17.3m
-  // pulseIn() returns microseconds.
+}
+
+float Median_filter(int n){
+  for(int i = 1; i < n; i++){
+    num[n - i] = num[n - i - 1];
+  }
+  num[0] = dist_raw;
+
+  if (n % 2 == 1) return num[n / 2];
+  else return (num[(n / 2) - 1] + num[n / 2]) / 2;
 }
